9f5d91f9ad44e6984f8779253ab599d557b2dd43
Will FP <william.favierparsons1@gmail.com>
2024-02-23 12:39:06 +0000

    Yep

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sustain-a-meal.willfp.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  output: 'standalone',
+  experimental: {
+    // Fixes Clerk before PR is merged
+    missingSuspenseWithCSRBailout: false,
+  },
+}
 
 export default nextConfig;
+
