54ffd9bd42792874beeacc4d19bd40e6b9828401
Will FP <william.favierparsons1@gmail.com>
2024-03-20 15:38:12 +0000

    Fixed dynamic

--- src/app/api/v1/leaderboard/route.ts
+++ src/app/api/v1/leaderboard/route.ts
@@ -1,6 +1,6 @@
 import {getLeaderboard} from "@/lib/leaderboard";
 
-export const dynamic = true;
+export const dynamic = "force-dynamic"
 
 export async function GET(request: Request) {
     return Response.json({
