2f9bc9ae7f8a176df5ba7208bbe3a16a993f3c75
Will FP <william.favierparsons1@gmail.com>
2024-03-20 15:51:31 +0000

    Added comment

--- src/app/api/v1/leaderboard/route.ts
+++ src/app/api/v1/leaderboard/route.ts
@@ -1,5 +1,8 @@
 import {getLeaderboard} from "@/lib/leaderboard";
 
+/*
+The route is marked as dynamic to ensure that the latest data is always shown to users.
+ */
 export const dynamic = "force-dynamic"
 
 export async function GET(request: Request) {
